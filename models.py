fec2476582e1e7fd270437803bd017f5a6675dbc
Brian Tu <tub4@uci.edu>
2022-06-05 14:44:50 -0700

    Restructured files

--- models.py
+++ models.py
@@ -13,28 +13,35 @@ from sklearn.model_selection import learning_curve
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 
-def model():
-    extracted = preprocessing.column_selection("data/train.csv")
-    X = extracted.drop(columns="Survived", axis=1)
-    Y = extracted["Survived"]
-
+def logRegression(X, Y):
     pol = PolynomialFeatures(2) # Added 
-
-    X_train, X_test, Y_train, Y_test = train_test_split(pol.fit_transform(X), Y, test_size=0.2)
 
     # rescale data in trainnig stage (decrease sensitivity of the model)
     sc = StandardScaler()
-    scaler = sc.fit(X_train)
-    trainX_scaled = scaler.transform(X_train)
-    testX_scaled = scaler.transform(X_test)
+    scaler = sc.fit(X)
+    trainX_scaled = scaler.transform(X)
 
     model = LogisticRegression(penalty="l2", max_iter=1000)
-    model.fit(trainX_scaled,Y_train)
+    model.fit(trainX_scaled,Y)
+
+    return model
+if __name__ == "__main__":
+    extracted = preprocessing.column_selection("data/train.csv")
+    X = extracted.drop(columns="Survived", axis=1)
+    Y = extracted["Survived"]
+    pol = PolynomialFeatures(2)
+
+    X_train, X_test, Y_train, Y_test = train_test_split(pol.fit_transform(X), Y, test_size=0.2)
+    model = logRegression(X_train, Y_train)
+    # rescale data in trainnig stage (decrease sensitivity of the model)
+    sc = StandardScaler()
+    scaler = sc.fit(X_train)
+    testX_scaled = scaler.transform(X_test)
 
     Y_pred = model.predict(testX_scaled)
 
+    # figure = plot_confusion_matrix(model, X_test, Y_test, display_labels=model.classes_)
+    # plt.show()
+
     print(classification_report(Y_pred,Y_test))
     print("Accuracy: {:.2f}".format(accuracy_score(Y_test, Y_pred)))
-
-if __name__ == "__main__":
-    model()
